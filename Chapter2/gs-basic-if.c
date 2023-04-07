f15418257c68e5b4f4a580810c7631e811c3364c
abhi ivaturi <abhi@mailbox.org>
2023-04-07 07:17:28 +0200

    gs vs if

--- Chapter2/gs-basic-if.c
+++ Chapter2/gs-basic-if.c
@@ -3,7 +3,7 @@
 int main(void){
 
     for (size_t i = 0; i < 5; ++i){
-        if (i){
+        if (i){ // skips the 0-index
             printf("> A[%zu] = %g \t\t %g^^2 = %g \n", i, A[i], A[i], A[i] * A[i]);
         }
     }
